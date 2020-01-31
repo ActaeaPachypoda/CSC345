cb3210be0da3643cac69a0cc712757b403c85a5d
Sarah Fondots <actaea.p@gmail.com>
2020-01-30 21:10:20 -0500

    finished isPositive

---
+++ Homework/Prog1.hs
@@ -0,0 +1,38 @@
+{- ##################################
+ Sarah Fondots
+ Homework 1.
+ ################################## -}
+
+module Prog1 where
+
+  isPositive :: Float -> Bool
+  isPositive n
+    | n >=0 = True
+    | otherwise = False
+
+dividesEvenlyByFive :: Integer -> Bool
+dividesEvenlyByFive _ = error "undefined"
+
+middle :: Integer -> Integer -> Integer -> Integer
+middle _ = error "undefined"
+
+nor :: Bool -> Bool -> Bool
+nor _ = error "undefined"
+
+triangleArea :: Integer -> Integer -> Float
+triangleArea _ = error "undefined"
+
+ceilingDecimal :: Float -> Float
+ceilingDecimal _ = error "undefined"
+
+letterGrade :: Integer -> String
+letterGrade _ = error "undefined"
+
+averageThree :: Integer -> Integer -> Integer -> Float
+averageThree _ = error "undefined"
+
+howManyAboveAverage :: Integer -> Integer -> Integer -> Integer
+howManyAboveAverage _ = error "undefined"
+
+howManyWithinThreshold :: Integer -> Integer -> Integer -> Float -> Integer
+howManyWithinThreshold _ = error "undefined"
