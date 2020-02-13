b7dd16c7faddd161e2018e6c6fb4f4b4f047b2e3
Sarah Fondots <actaea.p@gmail.com>
2020-02-12 19:44:37 -0500

    New homework 2 skeleton file

---
+++ Homework/Prog2.hs
@@ -0,0 +1,36 @@
+{- ##################################
+ Sarah Fondots
+ Homework 2
+ 2/12/2020
+ ################################## -}
+module Prog2 where
+
+threeDifferent :: Integer -> Integer -> Integer -> Bool
+threeDifferent _ = error "undefined"
+
+fourDifferent :: Integer -> Integer -> Integer -> Integer -> Bool
+fourDifferent _ = error "undefined"
+
+sum' :: Integer -> Integer
+sum' _ = error "undefined"
+
+asciisum :: String -> Integer
+asciisum _ = error "undefined"
+
+integerSqrt :: Integer -> Integer
+integerSqrt _ = error "undefined"
+
+orderTriple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+orderTriple _ = error "undefined"
+
+swap :: (Char, Char, Char, Char) -> (Char, Char, Char, Char)
+swap _ = error "undefined"
+
+negateTwoDigits :: [Integer] -> [Integer]
+negateTwoDigits _ = error "undefined"
+
+matches :: Integer -> [Integer] -> [Integer]
+matches _ = error "undefined"
+
+element :: Integer -> [Integer] -> Bool
+element _ = error "undefined"
